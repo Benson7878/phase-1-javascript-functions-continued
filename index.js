cdd10e45612c53bdf92eb6b4d2bdabb656d656e5
Benson7878 <bensonnyanchieo78@gmail.com>
2024-03-27 05:47:13 +0300

    Updated functions continued

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // code your solution here
-function saturdayFun(activity="roller-skate") {
-    return `This Saturday, I want to ${activity}!`
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will  ${activity}!`;
+}
+
+// Define function wrapAdjective
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
   }
-  let mondayWork = function(activity="go to the office") {
-    return `This Monday, I will ${activity}.`
-  }
-  let wrapAdjective = function(style="*") {
-    return function(adjective="special") {
-      return `You are ${style}${adjective}${style}!`
-    }
-  } 
+}
